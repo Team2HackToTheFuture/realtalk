3941a31bfc06afcfb0653817e00dc0bbca6d67b0
Scott Kelly <scottshamus@gmail.com>
2016-05-14 11:59:57 -0500

    updating search

--- app/models/lesson-plan.js
+++ app/models/lesson-plan.js
@@ -8,19 +8,35 @@ var LessonPlan =  Model.extend({
     groupSize: attr('number'),
     subject: attr('string'),
     materials: attr('string'),
-    activities: hasMany('activity')
+    activities: hasMany('activity'),
+    rating: attr('number'),
+
+    ratingStars: function() {
+        var ratingStars = '';
+        for(var x=0; x <this.get('rating'); x++) {
+            ratingStars += '<span class="glyphicon glyphicon-star"></span>';
+        }
+        return ratingStars;
+    }.property('rating')
 });
 
 
 LessonPlan.reopenClass({
-   FIXTURES: [
-    {
-      id: 1,
-      title: "Ferguson Violence"
+   FIXTURES: [{
+       id: 1,
+       title: "Ferguson Violence",
+       subject: "Social Studies",
+       groupSize: 10,
+       rating: 5,
+       materials: "5 Markers"
     },
    {
-      id: 2,
-      title: "Chance the Rapper's Impact on Modern Hip-Hop"
+        id: 2,
+        title: "Chance the Rapper's Impact on Modern Hip-Hop",
+        subject: "Music",
+        groupSize: 30,
+        rating: 4,
+        materials: "1 iPod"
     }
    ]
 });
