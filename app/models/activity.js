a7414b4ca613b360265e4706ddb3991aa962ae2f
Scott Kelly <scottshamus@gmail.com>
2016-05-14 14:07:46 -0500

    adding better search results

--- app/models/activity.js
+++ app/models/activity.js
@@ -2,9 +2,19 @@ import Model from 'ember-data/model';
 import { belongsTo } from 'ember-data/relationships';
 import attr from 'ember-data/attr';
 
-export default Model.extend({
+var Activity =  Model.extend({
     lessonPlan: belongsTo('lesson-plan'),
     name: attr('string'),
     content: attr('string'),
     type: attr('string')
+});
+
+
+Activity.reopenClass({
+   FIXTURES: [
+       {
+           "name": "Discuss Drugs and Violence",
+           "content": "<p>"
+       }
+   ]
 });
