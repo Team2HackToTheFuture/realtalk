66a8409aab56f4096a710f27493cadb604e8fc65
Scott Kelly <scottshamus@gmail.com>
2016-05-14 17:07:48 -0500

    lots of things

--- app/routes/search.js
+++ app/routes/search.js
@@ -10,6 +10,7 @@ export default Ember.Route.extend({
   model: function(params) {
     return   this.store.findAll('lesson-plan').then(function(lessons) {
         return lessons.filter(function(lesson) {
+          if(!lesson.get('title')) {return;}
           return lesson.get('title').includes(params.searchTerm);
         });
       });
