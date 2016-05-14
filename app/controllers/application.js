b961b3c66f8d7c0f8d571e6cb0f92d76693420f7
cosileone <cosi.leone@icloud.com>
2016-05-14 17:30:03 -0500

    remove stars once 'inside' app

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,4 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isLoggedIn: false,
+
+  removeBackground: function() {
+   if(this.get('isLoggedIn')) {
+     $('body').css('background-image', 'none');
+     $('body').css('background-color', 'white');
+     $('#title').css('text-shadow', 'none');
+     $('#title').css('color', 'grey');
+     $('#title').css('padding-top', '0px');
+   }
+ }.observes('isLoggedIn')
 });
