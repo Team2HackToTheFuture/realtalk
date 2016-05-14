f794e47b1e6de8b4dd02a63fe25725fc9ef27d75
Scott Kelly <scottshamus@gmail.com>
2016-05-14 15:14:48 -0500

    more improvements

--- app/routes/create-activity-detail.js
+++ app/routes/create-activity-detail.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
     model: function() {
-        return this.store.createRecord('activity');
+        return this.store.createRecord('activity', {
+            "name": "Donald Trump and 21st Century Politics"
+        });
     },
 });
